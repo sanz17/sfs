655143358009b52c7fcbd53adf473608b44e75c3
sanz17 <kundusanhita13@gmail.com>
2022-04-26 19:08:09 +0530

    Home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./CSS/Home.css";
 import Image from "react-bootstrap/Image";
 import { Row, Col } from "react-bootstrap";
+// import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 
+
+const theme = createTheme({
+  typography: {
+    // Tell MUI what the font-size on the html element is.
+    htmlFontSize: 5,
+  },
+});
 const Home = () => {
   return (
     <>
@@ -11,7 +21,9 @@ const Home = () => {
           <Image src="./img/imgtwo.jpg" thumbnail style={{ border: "none" }} />
         </Col>
         <Col>
-          Secure file storage system
+          <ThemeProvider theme={theme}>
+            <br /><br /><Typography><strong>Secure file storage system</strong></Typography>
+          </ThemeProvider>
         </Col>
       </Row>
     </>
